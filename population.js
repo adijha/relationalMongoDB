30481778efd80f4f72e9d980746f77ba6bf8996c
Aditya Kumar Jha <adityajha126@gmail.com>
2019-06-08 13:34:01 +0530

    list auther also

--- population.js
+++ population.js
@@ -43,7 +43,8 @@ async function createCourse(name, author) {
 async function listCourses() { 
   const courses = await Course
     .find()
-    .select('name');
+    .populate('author')
+    .select('name author'); 
   console.log(courses);
 }
 
