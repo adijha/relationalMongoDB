1e3634e65758e4e1921737ba634d82db148e3015
Aditya Kumar Jha <adityajha126@gmail.com>
2019-06-11 05:49:57 +0530

    adding author is mandatory

--- embedding.js
+++ embedding.js
@@ -14,7 +14,10 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author: authorSchema
+  author: {
+    type : authorSchema,
+    required : true
+  }
 }));
 
 async function createCourse(name, author) {
